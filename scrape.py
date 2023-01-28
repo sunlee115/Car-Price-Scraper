4913a0b414d409dc41b67e525f82b83496a32367
Sun Lee <sunlee115@gmail.com>
2023-01-28 16:48:42 -0600

    random changes

--- scrape.py
+++ scrape.py
@@ -2,13 +2,31 @@ import requests
 import time
 from bs4 import BeautifulSoup as bs
 
+"""
+what if the twitter bot everyday just tweeted the average price of a random car??
+honda = [s2000, civic, odyssey]
+bmw = [3-series, x5]
+etc...
+
+then everyday it could tweet:
+
+'Today, the average cost of a HONDA S2000 in city,state is $1234.56
+Gathered from truecar, cargurus, etc'
+
+
+and the city and state could change everyday too for fun?
+"""
 
 #url = 'https://www.truecar.com/used-cars-for-sale/listings/location-carrollton-tx/?searchRadius=25'
 test_url = 'https://www.truecar.com/used-cars-for-sale/listings/honda/s2000/location-carrollton-tx/?searchRadius=10'
 page = requests.get(test_url)
 soup = bs(page.content,'html.parser')
 
-search_result = soup.find_all("div",class_="heading-3 my-1 font-bold")
+
+# search_result = soup.find_all("div",class_="heading-3 my-1 font-bold")
+# using the bottom one bc it looks more official lol
+search_result=soup.find_all(attrs={"data-test": "vehicleCardPricingBlockPrice"})
+
 list_of_prices = []
 for x in search_result:
     price_float = float(x.text.replace("$","").replace(",",""))
